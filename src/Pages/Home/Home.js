c07710ffdb3fc5dc62b672f6201718be73f01433
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-05-22 03:55:14 +0600

    added a simple navbar

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className=" text-center text-4xl font-bold text-purple-400">Welcome to Dreamy Electric</h1>
+
+        </div>
+    );
+};
+
+export default Home;
