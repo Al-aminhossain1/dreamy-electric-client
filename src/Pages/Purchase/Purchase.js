c791ee1b2f993fb2c5e1dc0a4a1dc73adb0163b1
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-05-24 01:09:41 +0600

    send new order in database

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -8,29 +8,55 @@ const Purchase = () => {
     const { id } = useParams();
     const [user] = useAuthState(auth);
     const [tool, setTool] = useState({});
+    let errorMessage;
     useEffect(() => {
         const url = `http://localhost:5000/tool/${id}`;
         fetch(url)
             .then(res => res.json())
             .then(data => setTool(data))
     }, [id])
+    const confirmOrdered = event => {
+        event.preventDefault();
+        const user = event.target.user.value;
+        const email = event.target.email.value;
+        const phone = event.target.phone.value;
+        const order = event.target.order.value;
+        const address = event.target.address.value;
+        if (order < 10 || order > 50) {
+            return errorMessage = <p>minimum order at least 10 products</p>
+
+        }
+        const data = { user, email, address, phone, order }
+        console.log(data);
+        fetch('http://localhost:5000/order', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
     return (
-        <div className='flex justify-center'>
+        <div className='flex justify-center '>
             <div class="card w-96 bg-base-100 shadow-xl">
                 <div class="card-body">
                     <h2 class="text-center text-xl text-primary font-bold">{tool.name}</h2>
                     <p>{tool.description}</p>
                     <p>Available: {tool.available}</p>
-                    <input type="text" disabled value={user?.displayName} class="input input-bordered w-full max-w-xs" />
-                    <input type="email" disabled value={user?.email} class="input input-bordered w-full max-w-xs" />
-                    <input type="text" placeholder="Enter you Address" class="input input-bordered w-full max-w-xs" />
-                    <input type="text" placeholder="Phone Number" class="input input-bordered w-full max-w-xs" />
-
-                    <input type="number" disabled value={tool.available} class="input input-bordered w-full max-w-xs" />
-                    <input type="number" placeholder="Minimum order 10" class="input input-bordered w-full max-w-xs" />
-                    {/* <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" /> */}
-
-                    <button className='btn'>Order</button>
+                    <form onSubmit={confirmOrdered}>
+                        <input type="text" name='user' disabled value={user?.displayName} class="input my-2 input-bordered w-full max-w-xs" />
+                        <input type="email" name='email' disabled value={user?.email} class="input my-2 input-bordered w-full max-w-xs" />
+                        <input type="text" name='address' placeholder="Enter you Address" class="input my-2 input-bordered w-full max-w-xs" />
+                        <input type="text" name='phone' placeholder="Phone Number" class="input my-2 input-bordered w-full max-w-xs" />
+                        <input type="number" disabled value={tool.available} class="input my-2 input-bordered w-full max-w-xs" />
+                        <input type="number" name='order' placeholder="Minimum order 10" class="input my-2 input-bordered w-full max-w-xs" />
+                        {errorMessage}
+                        {errorMessage ? <input disabled type="submit" className='btn my-2 w-full max-w-xs' value="purchase" /> : <input type="submit" className='btn my-2 w-full max-w-xs' value="purchase" />}
+                    </form>
                 </div>
             </div>
 
