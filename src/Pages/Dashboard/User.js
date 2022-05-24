ac42a45bf62891835b2c63671a2a97dae6d294c4
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-05-25 04:30:14 +0600

    make admi api

--- src/Pages/Dashboard/User.js
+++ src/Pages/Dashboard/User.js
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const User = () => {
+    const [user] = useAuthState(auth);
     const [users, setUsers] = useState([]);
     useEffect(() => {
         fetch('http://localhost:5000/user')
             .then(res => res.json())
             .then(data => setUsers(data))
     }, [])
+    const handelMakeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${user.email}`, {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
     return (
         <div>
             <h1>Total User:{users.length}</h1>
@@ -23,14 +38,13 @@ const User = () => {
                         </tr>
                     </thead>
                     <tbody>
-
                         {
                             users.map((user, index) => <tr>
                                 <th>{index + 1}</th>
                                 <td>{user.email}</td>
                                 <td>{user.address}</td>
                                 <td>{user.phone}</td>
-                                <td><button className='btn btn-xs'>Make Admin</button></td>
+                                <td><button onClick={handelMakeAdmin} className='btn btn-xs'>Make Admin</button></td>
                             </tr>)
                         }
 
