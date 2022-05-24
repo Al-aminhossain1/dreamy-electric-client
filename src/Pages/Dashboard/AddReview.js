8a5d7727d23e237aa6de29aaf98294a9a09aa3a1
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-05-24 07:20:21 +0600

    added review page design

--- src/Pages/Dashboard/AddReview.js
+++ src/Pages/Dashboard/AddReview.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
 
 const AddReview = () => {
+    const [user] = useAuthState(auth);
+    const handelAddReview = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const rating = event.target.rating.value;
+        const description = event.target.description.value;
+        const review = { name, rating, description };
+        console.log(review);
+        toast("Thanks for your Review")
+        event.target.reset();
+    }
     return (
-        <div>
-            <h1>This is add review page</h1>
+        <div className='flex justify-center '>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-center text-xl text-cyan-500 font-semibold">Please Give your review</h2>
+                    {/* <p>{tool.description}</p> */}
+                    <form onSubmit={handelAddReview}>
+                        <input type="text" name='name' disabled value={user?.displayName} class="input my-2 input-bordered w-full max-w-xs" />
+                        <input type="text" name='rating' placeholder="review rating" class="input my-2 input-bordered w-full max-w-xs" />
+                        <textarea name='description' class="textarea textarea-bordered w-full max-w-xs" placeholder="Review"></textarea>
+                        <input type="submit" value="Review" class=" btn my-2 input-bordered w-full max-w-xs" />
+                    </form>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/Pages/Dashboard/MyOrder.js
+++ src/Pages/Dashboard/MyOrder.js
@@ -42,7 +42,6 @@ const MyOrder = () => {
                             </tr>)
                         }
 
-
                     </tbody>
                 </table>
             </div>
